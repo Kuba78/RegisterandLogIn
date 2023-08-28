e6909d07bd2925b1cfbdc709d61fb09e5e073f38
Kuba78 <53141999+Kuba78@users.noreply.github.com>
2023-08-28 15:15:47 +0200

    Renamed folder Add User Object User

--- src/DAO/User.java
+++ src/DAO/User.java
@@ -5,13 +5,93 @@ import java.util.Date;
 
 public class User {
 
+    public enum Sex{
+        male,
+        female
+    }
     private int id;
     private String firstname;
     private String lastname;
     private String email;
     private Date birthday;
     private Timestamp registrationDate;
+
+    /**
+     * Hashed text, no plain text password.
+     */
     private String password;
-    private Enum sex;
+    private Sex sex;
     private boolean mailSent;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public Timestamp getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(Timestamp registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Sex getSex() {
+        return sex;
+    }
+
+    public void setSex(Sex sex) {
+        this.sex = sex;
+    }
+
+    public boolean isMailSent() {
+        return mailSent;
+    }
+
+    public void setMailSent(boolean mailSent) {
+        this.mailSent = mailSent;
+    }
 }
