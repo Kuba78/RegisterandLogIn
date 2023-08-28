21d5ad4c43e664ca2145dee3c5bcf2658e0594c7
Kuba78 <53141999+Kuba78@users.noreply.github.com>
2023-08-28 15:27:09 +0200

    DAO User user hinzufügen und prüfen ob er schon existiert

--- src/database/InMemoryDatabase.java
+++ src/database/InMemoryDatabase.java
@@ -1,4 +1,26 @@
 package database;
 
+import DAO.User;
+
+
+import java.util.ArrayList;
+
 public class InMemoryDatabase {
+
+    private ArrayList<User> userDatabase;
+
+    public InMemoryDatabase(){
+        this.userDatabase = new ArrayList<>();
+    }
+/* Adds new user to the in memory database. If not empty.
+@param user User Object
+return boolean if user successfully added, false if null or not added.
+* */
+    public boolean addUser(User user){
+        if(user !=null){
+            //ToDo check if user already exists
+            return this.userDatabase.add(user);
+        }
+        return false;
+    }
 }
